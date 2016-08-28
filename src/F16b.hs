29586a0b59efbcc6cc6e957a754364d9ea9da08c
Erik Rantapaa <erantapaa@gmail.com>
2016-08-27 19:05:52 -0500

    Add F16b - ekmett's GF code

--- src/F16b.hs
+++ src/F16b.hs
@@ -2,10 +2,11 @@
 
 -- based on ed kemmett's code at: https://www.schoolofhaskell.com/user/edwardk/parallel-crc
 
-module F16c where
+module F16b where
 
 import Data.Bits
 import Data.Word
+import Data.List (foldl')
 
 -- show
 newtype GF = GF { runGF :: Word32 } deriving (Eq,Show,Read,Bits)
@@ -20,6 +21,11 @@ poly = GF 0xd400
 xtimes :: GF -> GF
 xtimes c = unsafeShiftR c 1 + if testBit c 0 then poly else 0
 
+intToGF :: Bits a => a -> GF
+intToGF x = GF y
+  where y = foldl' go 0 [0..15]
+          where go s k = if testBit x k then setBit s (15-k) else s
+
 instance Num GF where
   (+) = xor
   (-) = xor
@@ -28,4 +34,5 @@ instance Num GF where
   negate = id
   abs = id
   signum = fromIntegral . signum . runGF
+  fromInteger = intToGF
 
